ae4a23320a8a9409fb7df3310558073c0a0b9a9a
wan6sta <wan6sta@gmail.com>
2023-01-27 17:58:52 +0300

    Initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MyTop</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='use-credentials'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
